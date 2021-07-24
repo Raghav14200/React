b69af910f2472ef40660d3a5444c91f4b5517224
Raghavendra Achar C <raghav142001@gmail.com>
2021-07-24 18:49:27 +0530

    publish github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import {Component} from 'react';
+import Monster from './Components/monster_template';
+
+
+class App extends Component{
+  constructor(){
+
+    super();
+    this.state={
+        monster:[],
+        search:''
+    };
+}
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(fact => {
+          this.setState(
+          {
+          monster:fact
+        }
+        )
+    })
+  }
+
+  updatesearch = (e)=>{
+    this.setState({search:e.target.value})
+  }
+
+  render(){
+    return(
+      <div class="align">
+      <h1>Monter Hunter</h1>
+      <input type='search' placeholder='Search' onChange={this.updatesearch}/>
+          <div className='grid'>
+        {this.state.monster.filter((data)=>{
+          return data.name.toLowerCase().includes(this.state.search.toLowerCase())
+        }).map( data2 => {
+          return <Monster data={data2}/>
+        })}
+          </div>
+      </div>
+    )
+  }
 }
 
 export default App;
